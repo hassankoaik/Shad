277273a5be3ead2368a046d411e4dc63c340911b
Olo2020 <hassan.keaik@lau.edu>
2024-03-15 12:36:20 +0200

    new

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -2,16 +2,33 @@
 import DropzoneComponents from 'react-dropzone'
 
 function Dropzone() {
+const maxsize=20971520
+
+
+
   return (
-    <DropzoneComponents onDrop={acceptedFiles => console.log(acceptedFiles)}>
-    {({getRootProps, getInputProps}) => (
+    <DropzoneComponents minSize={0} maxSize={maxsize} onDrop={acceptedFiles => console.log(acceptedFiles)}>
+    {({getRootProps, getInputProps, isDragActive,isDragReject,fileRejections}) => 
+    
+    {
+
+      const isFileTooLarge=fileRejections.length>0 && fileRejections[0].file.size>maxsize;
+      return(
     <section>
       <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+       <input {...getInputProps()}/> 
+        {!isDragActive && "Click here or  drop a file to upload "}
+        {isDragActive && !isDragReject && "Drop to upload this File!"}
+        {isDragReject && "File type not accepted"}
+        {isFileTooLarge && (
+          <div className='text-danger mt-2 '>
+            File is too Large 
+          </div>
+        )}
+
       </div>
     </section>
-  )}
+  )}}
 </DropzoneComponents>
   )
 }
